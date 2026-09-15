2d0ad1d97309d1cf8a6730c94849649ea50a2337
Xt3v3n <stevenzuleta2007@gmail.com>
2026-09-15 07:15:18 -0500

    Limpio archivos heredados de main

--- contarcaracteres.py
+++
@@ -1,9 +0,0 @@
-def contar_caracteres(s, c):
-    if len(s) == 0:
-        return 0
-
-    cuenta = 1 if s[0] == c else 0
-
-    return cuenta + contar_caracteres(s[1:], c)
-
-print(contar_caracteres("alicia", "a"))
--- factorial.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def factorial(n):
-    if n <= 1:
-        return 1
-
-    return n * factorial(n-1)
