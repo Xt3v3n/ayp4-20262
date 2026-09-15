5ec8d65ac57ea516212ff8fe7f1e8b5992f6fc79
Xt3v3n <stevenzuleta2007@gmail.com>
2026-09-15 07:04:25 -0500

    Clone el repo de algoritmos de Isaac

--- expresionesregulares.py
+++ expresionesregulares.py
@@ -1,12 +1,15 @@
 import re
 
-texto = "Estoy usando Python"
+texto = "gato, geto, gito, goto, guto, g4to, g--to"
 
 """resultado = re.match("Estoy", texto)
 print(resultado)"""
 
 """resultado = re.search("Python", texto)
-print(resultado)"""
+print(resultado)
 
 resultado = re.findall("o", texto)
+print(resultado)"""
+
+resultado = re.findall(r"g..to", texto) # El punto es un metacaracter / comodin
 print(resultado)
